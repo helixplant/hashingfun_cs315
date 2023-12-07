fa43b536f1655c59ef992ad83f0be41fe482ae7e
h0t5auc3 <113943417+h0t5auc3@users.noreply.github.com>
2023-12-07 14:00:42 -0500

    initial

---
+++ WordCounter.java
@@ -0,0 +1,22 @@
+/**
+ * Implements a word frequency counter using a binary search tree and a heap.
+ */
+
+public class WordCounter {
+
+    //DECLARE CLASS VARIABLES HERE
+    // LPHash lph = new LPHash(4);
+
+    public static void countWords(String [] words) {
+    LPHash lph = new LPHash();
+
+        //Impelement your solution here
+       
+    }
+
+
+    public static void main(String [] args) {
+        String input = "input.txt";
+        WordCounter.countWords(InputReader.parseInputFile(input));
+    }
+}
